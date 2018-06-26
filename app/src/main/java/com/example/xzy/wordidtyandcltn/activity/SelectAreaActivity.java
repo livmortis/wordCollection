88242dd9c9e1b86ab38c328adec68d2aa147c713
xzy <xzy@xzydeMacBook-Pro.local>
2018-06-27 00:24:37 +0800

    add http request

--- app/src/main/java/com/example/xzy/wordidtyandcltn/activity/SelectAreaActivity.java
+++ app/src/main/java/com/example/xzy/wordidtyandcltn/activity/SelectAreaActivity.java
@@ -40,6 +40,7 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLEncoder;
@@ -323,11 +324,11 @@ public class SelectAreaActivity extends BaseActivity implements View.OnClickList
                     URL url = null;
                     url = new URL("http://webapi.xfyun.cn/v1/service/v1/ocr/general");
                     urlConnection = (HttpURLConnection) url.openConnection();//打开http连接
-                    urlConnection.setConnectTimeout(3000);//连接的超时时间
+                    urlConnection.setConnectTimeout(5000);//连接的超时时间
                     urlConnection.setUseCaches(false);//不使用缓存
                     //urlConnection.setFollowRedirects(false);是static函数，作用于所有的URLConnection对象。
                     urlConnection.setInstanceFollowRedirects(true);//是成员函数，仅作用于当前函数,设置这个连接是否可以被重定向
-                    urlConnection.setReadTimeout(3000);//响应的超时时间
+                    urlConnection.setReadTimeout(5000);//响应的超时时间
                     urlConnection.setDoInput(true);//设置这个连接是否可以写入数据
                     urlConnection.setDoOutput(true);//设置这个连接是否可以输出数据
                     urlConnection.setRequestMethod("POST");//设置请求的方式
@@ -351,10 +352,14 @@ public class SelectAreaActivity extends BaseActivity implements View.OnClickList
 
 
 //                    urlConnection.connect();// 连接，从上述至此的配置必须要在connect之前完成，实际上它只是建立了一个与服务器的TCP连接
-                    JSONObject json = new JSONObject();//创建json对象
-                    json.put("image", URLEncoder.encode(photoAfterB64, "UTF-8"));//使用URLEncoder.encode对特殊和不可见字符进行编码
+//                    JSONObject json = new JSONObject();//创建json对象
+//                    json.put("image", URLEncoder.encode(photoAfterB64, "UTF-8"));//使用URLEncoder.encode对特殊和不可见字符进行编码
+//
+//                    String jsonstr = json.toString();//把JSON对象按JSON的编码格式转换为字符串
 
-                    String jsonstr = json.toString();//把JSON对象按JSON的编码格式转换为字符串
+
+
+
                     //-------------使用字节流发送数据--------------
                     //OutputStream out = urlConnection.getOutputStream();
                     //BufferedOutputStream bos = new BufferedOutputStream(out);//缓冲字节流包装字节流
@@ -363,16 +368,27 @@ public class SelectAreaActivity extends BaseActivity implements View.OnClickList
                     //bos.flush();//刷新缓冲区，发送数据
                     //out.close();
                     //bos.close();
-                    //------------字符流写入数据------------
-                    OutputStream out = urlConnection.getOutputStream();//输出流，用来发送请求，http请求实际上直到这个函数里面才正式发送出去
-                    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(out));//创建字符流对象并用高效缓冲流包装它，便获得最高的效率,发送的是字符串推荐用字符流，其它数据就用字节流
-                    bw.write(jsonstr);//把json字符串写入缓冲区中
-                    bw.flush();//刷新缓冲区，把数据发送出去，这步很重要
-                    out.close();
-                    bw.close();//使用完关闭
+
+                    //------------字符流写入数据，wordCollection更换------------
+//                    OutputStream out = urlConnection.getOutputStream();//输出流，用来发送请求，http请求实际上直到这个函数里面才正式发送出去
+//                    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(out));//创建字符流对象并用高效缓冲流包装它，便获得最高的效率,发送的是字符串推荐用字符流，其它数据就用字节流
+//                    bw.write(jsonstr);//把json字符串写入缓冲区中
+//                    bw.flush();//刷新缓冲区，把数据发送出去，这步很重要
+//                    out.close();
+//                    bw.close();//使用完关闭
                     Log.d("xzyupload", 3333333 + "");
 
-                    if (urlConnection.getResponseCode() == HttpURLConnection.HTTP_OK) {//得到服务端的返回码是否连接成功
+
+                    //-------demo使用 ---------
+                    PrintWriter out = new PrintWriter(urlConnection.getOutputStream());
+                    out.print("image=" + photoAfterB64);
+                    out.flush();
+
+
+
+
+
+//                    if (urlConnection.getResponseCode() == HttpURLConnection.HTTP_OK) {//得到服务端的返回码是否连接成功
                         //------------字节流读取服务端返回的数据------------
                         //InputStream in = urlConnection.getInputStream();//用输入流接收服务端返回的回应数据
                         //BufferedInputStream bis = new BufferedInputStream(in);//高效缓冲流包装它，这里用的是字节流来读取数据的，当然也可以用字符流
@@ -438,11 +454,11 @@ public class SelectAreaActivity extends BaseActivity implements View.OnClickList
 //                }
 
 
-                    } else {
-                        Log.d("xzyupload", "连接失败了哦" + "");
-                        mHandler.sendEmptyMessage(UPLOAD_FAILD);
-
-                    }
+//                    } else {
+//                        Log.d("xzyupload", "连接失败了哦" + "");
+//                        mHandler.sendEmptyMessage(UPLOAD_FAILD);
+//
+//                    }
                 } catch (Exception e) {
                     mHandler.sendEmptyMessage(UPLOAD_FAILD);
 
